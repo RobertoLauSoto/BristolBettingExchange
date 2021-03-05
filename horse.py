d937b15a682359318052e1e5fe08fbde57afb89b
RobertoLauSoto <roberto.lausoto@gmail.com>
2021-03-05 14:12:21 +0000

    distance-time graph visualiser

--- horse.py
+++ horse.py
@@ -4,20 +4,21 @@ import sys
 
 class Horse:
     def __init__(self, name, start, position, distance, time):
-        self.name           = name # identity of the horse/competitor
-        self.startPosition  = start # starting position of the competitor
-        self.currPosition   = position # current position of the competitor
-        self.currDistance   = distance # current distance run by competitor (metres)
-        self.currTime       = time # current time taken by competitor (seconds)
-        self.minSpeed       = 0 # minimum speed of competitor
-        self.maxSpeed       = 0 # maximum/top speed of competitor
-        self.currSpeed      = 0 # current speed of the competitor (metres/second)
-        self.prevSpeed      = 0 # speed in previous time step, used to calculate acceleration
-        self.acceleration   = 0 # current acceleration of the competitor (m/s2)
-        self.prefs          = None # vector for inital preference factors of competitor to help calculate step-size
-        self.resp = 1 # factor determining responsiveness of competitor, 1 = 100% responsive 
-        self.groundLost     = 1 # factor determinining the ground lost by a competitor due to interfernce, 1 = no delay
-        self.state         = None # currrent state of competitor (Running/Finished/DNF)
+        self.name            = name # identity of the horse/competitor
+        self.startPosition   = start # starting position of the competitor
+        self.currPosition    = position # current position of the competitor
+        self.currDistance    = distance # current distance run by competitor (metres)
+        self.currTime        = time # current time taken by competitor (seconds)
+        self.minSpeed        = 0 # minimum speed of competitor
+        self.maxSpeed        = 0 # maximum/top speed of competitor
+        self.currSpeed       = 0 # current speed of the competitor (metres/second)
+        self.prevSpeed       = 0 # speed in previous time step, used to calculate acceleration
+        self.acceleration    = 0 # current acceleration of the competitor (m/s2)
+        self.prefs           = None # vector for inital preference factors of competitor to help calculate step-size
+        self.resp            = 1 # factor determining responsiveness of competitor, 1 = 100% responsive 
+        self.groundLost      = 1 # factor determinining the ground lost by a competitor due to interfernce, 1 = no delay
+        self.distanceHistory = []
+        self.state           = None # currrent state of competitor (Running/Finished/DNF)
 
     def __str__(self):
         return '[ID %s startPosition %s currPosition %s currDistance %s currSpeed %s currTime %s resp %s groundLost %s state %s]' \
--- race.py
+++ race.py
@@ -1,8 +1,8 @@
 import numpy as np
 import time
 import sys
-import pandas
-import matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
 from horse import Horse
 class Race:
     def __init__(self, name, distance, numHorses):
@@ -28,35 +28,50 @@ class Race:
         horses = [None] * self.numHorses # array of Horse objects representing the starting horses
         for i in range(0,self.numHorses):
             horses[i] = Horse(i+1, 1, 1, 0, 0) # each horse has a number ID starting from 1 - could be changed to random string names
-            horses[i].minSpeed = np.random.uniform(1.0, 5.0)
-            horses[i].maxSpeed = np.random.uniform(self.maxTopSpeed / 2, self.maxTopSpeed)
+            horses[i].minSpeed = np.random.uniform(1.0, 5.0) # uniform distribution between 1 and 5 m/s to determine minimum speed
+            horses[i].maxSpeed = np.random.uniform(self.maxTopSpeed / 2, self.maxTopSpeed) # uniform distribution between ~12.2 and 24.5872 m/s(world record speed for a horse) to determine maximum speed
             # print(horses[i])
         return horses
     
+    def generateForwardStep(self):
+        pass
+    
+    def plotRaceGraph(self, horses):
+        fig, ax = plt.subplots() # generate figure with subplots
+        for i in range(len(horses)):
+            ax.plot(range(0, len(horses[i].distanceHistory)), horses[i].distanceHistory, label='Horse {}'.format(horses[i].name)) # plot each horses distance-time line graph 
+        legend = ax.legend(loc='upper left')
+        plt.xlabel('Time (seconds)')
+        plt.ylabel('Distance (metres)')
+        plt.title('Distance-Time graph for {}'.format(self.id))
+        plt.show()
+        
+
     def determineCurrentStandings(self):
-        self.currStandings.sort(key=lambda x: [x.currTime, -x.currDistance])
-        for i in range(0, len(self.currStandings)):
-            self.currStandings[i].currPosition = i+1
-            print(self.currStandings[i])
-        self.currStandings.clear()
+        self.currStandings.sort(key=lambda x: [x.currTime, -x.currDistance]) # sort current standings of horses by time (asc.) and then by distance (dist.) if equal time
+        for i in range(len(self.currStandings)):
+            self.currStandings[i].currPosition = i+1 # update position
+            # print(self.currStandings[i])
+        self.currStandings.clear() # clear array for next iteration
 
     def determineFinalPlacings(self):
-        self.finalStandings.sort(key=lambda x: [x.currTime, -x.currDistance])
-        for i in range(0, len(self.finalStandings)):
-            self.finalStandings[i].currPosition = i+1
-        self.winner.append(self.finalStandings[0])
-        self.top3.extend([self.finalStandings[0], self.finalStandings[1], self.finalStandings[2]])
+        self.finalStandings.sort(key=lambda x: [x.currTime, -x.currDistance]) # sort final standings of horses by time (asc.) and then by distance (dist.) if equal time
+        for i in range(len(self.finalStandings)):
+            self.finalStandings[i].currPosition = i+1 # place final position
+        self.winner.append(self.finalStandings[0]) # get winner
+        self.top3.extend([self.finalStandings[0], self.finalStandings[1], self.finalStandings[2]]) # get top 3 placed
 
     def runRace(self):
         horses   = self.createHorses() # create competitors
         time     = 0 # current race time in 'seconds'
         timestep = 1 # 1 second
-        while(self.state != 'Finished'):
+        while(self.state != 'Finished'): # while race is in running
             time += timestep # race time increases
             self.determineCurrentStandings()
-            for i in range(0, self.numHorses):
+            for i in range(self.numHorses): # for each horse
                 self.currStandings.append(horses[i])
                 if horses[i].state != 'Finished' and horses[i].state != 'DNF':
+                    horses[i].distanceHistory.append(horses[i].currDistance)
                     horses[i].state = 'Running' # horse is in running
                     horses[i].currTime = time 
                     horses[i].prevSpeed = horses[i].currSpeed # record previous speed
@@ -70,6 +85,7 @@ class Race:
                     if len(self.finalStandings) == self.numHorses: # all horses have finished
                         self.determineFinalPlacings()
                         self.state = 'Finished'
+                        self.plotRaceGraph(horses)
                         # print(len(self.finalStandings))
                         # print(self.numHorses)
                         # print(self.state)
@@ -78,11 +94,12 @@ class Race:
                     continue
                 
 if __name__ == "__main__":
-    testRace = Race("testRace", 1000, 8)
+    testRace = Race("Test Race", 1000, 8)
     testRace.runRace()
-    for i in range(0, len(testRace.winner)):
-        print('Winner ' + str(testRace.winner[i]))
-    for i in range(0, len(testRace.top3)):
-        print('Placed in ' + str(i+1) + ' place ' + str(testRace.top3[i]))
-    for i in range(0, len(testRace.finalStandings)):
+    for i in range(len(testRace.winner)):
+        print('Winner {}'.format(testRace.winner[i]))
+    for i in range(len(testRace.top3)):
+        print('Placed in {0} place {1}'.format(i+1, testRace.top3[i]))
+    for i in range(len(testRace.finalStandings)):
         print(testRace.finalStandings[i])
+    # testRace.plotRaceGraph(testRace.finalStandings)
